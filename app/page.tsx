f40c4f4946a00ff202f36974bca92a303cf9843d
sakkke <84666033+sakkke@users.noreply.github.com>
2024-03-29 04:44:16 +0000

    feat: implement home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
 export default function Home() {
-  return <main />;
+  return (
+    <main>
+      <p>this is my minimal profile :D</p>
+      <p>
+        hi! i&apos;m sakkke, a random person who is always doing creative works.
+        i&apos;m currently doing the following:
+      </p>
+      <ul>
+        <li>creating os</li>
+        <li>creating apps</li>
+        <li>creating websites</li>
+        <li>creating videos</li>
+        <li>creating musics</li>
+        <li>drawing logos</li>
+        <li>drawing illustrations</li>
+        <li>operating servers</li>
+      </ul>
+      <p>
+        my to-do list is always overflowing :P if you are interedted, please
+        contact me at <a href="mailto:sakkke@sakkke.dev">sakkke@sakkke.dev</a>!
+      </p>
+    </main>
+  );
 }
