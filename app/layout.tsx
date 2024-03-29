2a00e6c36b2fc0a32f219c50994e5594bf55fda3
sakkke <84666033+sakkke@users.noreply.github.com>
2024-03-29 12:20:30 +0000

    feat: change min-h-screen to min-h-dvh

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${jetBrainsMono.className} terminal`}>
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-dvh">
           <div className="container">
             <Header />
           </div>
