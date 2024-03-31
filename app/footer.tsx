147879f6e41b60d60f2d15e1a90dd824b50204b8
sakkke <84666033+sakkke@users.noreply.github.com>
2024-03-31 05:42:04 +0000

    feat: integrate GitHub and Zenn into footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,11 +1,16 @@
 const user = {
   name: "sakkke",
   email: "sakkke@sakkke.dev",
+  github: "https://github.com/sakkke",
+  zenn: "https://zenn.dev/sakkke",
 };
 
 export default function Footer() {
   return (
     <footer>
+      <p>
+        <a href={user.github}>GitHub</a> | <a href={user.zenn}>Zenn</a>
+      </p>
       <p>
         copyright &copy; 2024-present {user.name} &lt;
         <a href={`mailto:${user.email}`}>{user.email}</a>&gt;
