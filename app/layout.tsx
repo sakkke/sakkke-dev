68c19b0c4a240e3f234ec1c465f9771f6361cdd9
sakkke <84666033+sakkke@users.noreply.github.com>
2024-04-01 05:00:22 +0000

    fix: include terminal.css in override.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-import "terminal.css";
 import "./override.css";
 import Header from "./header";
 import Footer from "./footer";
