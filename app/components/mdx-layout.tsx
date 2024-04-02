8b9eda4ee4d46858e85ee48e6b44a7102177dd52
sakkke <84666033+sakkke@users.noreply.github.com>
2024-04-02 05:48:40 +0000

    feat: change metadata background color

--- app/components/mdx-layout.tsx
+++ app/components/mdx-layout.tsx
@@ -13,13 +13,15 @@ interface Props extends Metadata {
 export default function MdxLayout({ children, created, updated }: Props) {
   return (
     <>
-      <header className="container">
-        <p className="text-[var(--ctp-macchiato-overlay2)]">
-          created: {created}
-          <br />
-          updated: {updated}
-        </p>
-      </header>
+      <div className="bg-[var(--ctp-macchiato-surface0)]">
+        <header className="container">
+          <p className="text-[var(--ctp-macchiato-overlay2)]">
+            created: {created}
+            <br />
+            updated: {updated}
+          </p>
+        </header>
+      </div>
       <div className="container">
         <main>{children}</main>
         <div className="flex flex-row-reverse">
