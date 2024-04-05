5926af6aba9728e46c426f70810646acc84509a9
sakkke <84666033+sakkke@users.noreply.github.com>
2024-04-05 07:45:22 +0000

    feat: organize navigation links

--- app/header.tsx
+++ app/header.tsx
@@ -19,90 +19,10 @@ export default function Header() {
         <ul>
           <li>
             <Link
-              href="/blog"
-              className={`menu-item ${pathname === "/blog" ? "active" : ""}`}
-            >
-              blog
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/vlog"
-              className={`menu-item ${pathname === "/vlog" ? "active" : ""}`}
-            >
-              vlog
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/music"
-              className={`menu-item ${pathname === "/music" ? "active" : ""}`}
-            >
-              music
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/illustrations"
-              className={`menu-item ${pathname === "/illustrations" ? "active" : ""}`}
-            >
-              illustrations
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/photos"
-              className={`menu-item ${pathname === "/photos" ? "active" : ""}`}
-            >
-              photos
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/community"
-              className={`menu-item ${pathname === "/community" ? "active" : ""}`}
-            >
-              community
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/about"
-              className={`menu-item ${pathname === "/about" ? "active" : ""}`}
-            >
-              about me
-            </Link>
-          </li>
-          <li>
-            <Link
               href="/projects"
               className={`menu-item ${pathname === "/projects" ? "active" : ""}`}
             >
               projects
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/tools"
-              className={`menu-item ${pathname === "/tools" ? "active" : ""}`}
-            >
-              tools
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/skills"
-              className={`menu-item ${pathname === "/skills" ? "active" : ""}`}
-            >
-              skills
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/experience"
-              className={`menu-item ${pathname === "/experience" ? "active" : ""}`}
-            >
-              experience
             </Link>
           </li>
           <li>
